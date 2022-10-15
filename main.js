fd5bde16ef07cbcee8106a999968cce416a972b2
Ilya Eletskiy <ilarelet@gmail.com>
2022-10-15 17:14:48 +0300

    Change of color on mouse hover added.

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const container = document.querySelector("#container");
-
 const measure = 16*16;
 
+//creating divs
 for (let i=0; i<measure; i++) {
     let childDiv= document.createElement('div');
     container.appendChild(childDiv)
     childDiv.textContent =`This is a div number ${i+1}!`
+    childDiv.classList.add('cell')
 }
+//setting up the color changing event
+const cells = document.querySelectorAll('.cell')
+cells.forEach((childDiv) => {
+    childDiv.addEventListener('mouseenter',(e) => {
+        e.target.style.backgroundColor = 'red';
+    })
+})
