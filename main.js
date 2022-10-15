1d09ee6d3a16e3d075626bd002c293513a3959bb
Ilya Eletskiy <ilarelet@gmail.com>
2022-10-15 20:19:34 +0300

    Sketch updating relized

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 function initSketch(size){
-    const container = document.querySelector("#container");
-    //creating divs
-    for (let i=0; i<size-1; i++) {
+    //creating cells
+    for (let i=0; i<=(size**2)-1; i++) {
         let childDiv= document.createElement('div');
         container.appendChild(childDiv);
         childDiv.classList.add('cell');
@@ -9,25 +8,37 @@ function initSketch(size){
     //setting up the color changing event
     const cells = document.querySelectorAll('.cell');
     cells.forEach((cell) => {
+        //recalculating the width of the cells
+        cell.style.flexBasis = `${(500/(size))}px`;
         cell.addEventListener('mouseenter',(e) => {
             e.target.style.backgroundColor = 'red';
         })
     })
 }
 
+//deleting all the existing cells
+function clearSketch(){
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {container.removeChild(cell)});
+}
 
-const baseSize = 16*16;
+const container = document.querySelector("#container");
+//creating a basic 16x16 sketch
+const baseSize = 16;
 initSketch(baseSize);
 
+//setting up the RESIZE button
 const sizeButton = document.querySelector("#resize");
 sizeButton.addEventListener('click', ()=>{
     let newSize = prompt('Enter a new size of the sketch!');
     newSize=Number(newSize);
+    //checking if a valid number was entered
     if (newSize<=0 || newSize>100|| !Number.isInteger(newSize)){
         alert('PLease, enter a valid number below 100')
     }
     else{
-        newSize=newSize**2;
+        //resetting the sketch
+        clearSketch();
         initSketch(newSize);
     }
 })
