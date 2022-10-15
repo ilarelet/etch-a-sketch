63f221124aff6fbb20d8febac7db654eb41909a5
Ilya Eletskiy <ilarelet@gmail.com>
2022-10-15 17:39:47 +0300

    flexbox instead of CSS grid

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const measure = 16*16;
 for (let i=0; i<measure; i++) {
     let childDiv= document.createElement('div');
     container.appendChild(childDiv)
-    childDiv.textContent =`This is a div number ${i+1}!`
     childDiv.classList.add('cell')
 }
 //setting up the color changing event
